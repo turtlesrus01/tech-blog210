a4f58e2153e7771af21b97ead21104b88546f107
Green Camper <stefan.palacios@gmail.com>
2023-05-01 15:55:57 -0500

    Added code for Blog model

---
+++ models/Blog.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { title } = require('process');
+//Import model middleware
+class Blog extends Model {};
+//Blog init
+Blog.init(
+  {
+    //id 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //title
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    //description
+    description: {
+      type: DataTypes.STRING,
+    },
+    //date_created
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    //user_id
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      }
+    },
+  },
+  {
+    sequelize,
+    timeStamps: false,
+    freezeTableName: true,
+    underScored: true,
+    modelName: 'blog'
+  }
+);
+
+module.export = Blog;
