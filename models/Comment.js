9314472f7f930b627b0a6167a8ff05c51bd32544
Green Camper <stefan.palacios@gmail.com>
2023-05-01 18:29:42 -0500

    Completed code for Comment model

---
+++ models/Comment.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//Import model middleware
+class Comment extends Model {};
+
+//Blog init
+Comment.init(
+  {
+    //id 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //description
+    description: {
+      type: DataTypes.STRING,
+    },
+    //date_created
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    //user_id
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      }
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog',
+        key: 'id',
+      }
+    }
+  },
+  {
+    sequelize,
+    timeStamps: false,
+    freezeTableName: true,
+    underScored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
