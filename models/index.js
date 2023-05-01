eb8c865c9942636533628c59a75c320e7d6e4acb
Green Camper <stefan.palacios@gmail.com>
2023-05-01 15:06:51 -0500

    Added comments to model/index.js

--- models/index.js
+++ models/index.js
@@ -1,32 +1,33 @@
+//import model files
 const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
-
-User.hasMany(Project, {
+//Relating User to Blog
+User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
-
+//Relating Blog to User
 Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
-
+//Relating User to Comments
 User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
-
+//Relating Blogs to Comments
 Blog.hasMany(Comment, {
   foreignKey: 'blog_id',
   onDelete: 'CASCADE'
 });
-
+//Relating Comments to Users
 Comment.belongsTo(User, {
   foreignKey: 'user_id'
 });
-
+//Relating Comments to Blogs
 Comment.belongsTo(Blog, {
   foreignKey: 'blog_id'
 });
-
+//export model relational data
 module.exports = { User, Blog, Comment };
