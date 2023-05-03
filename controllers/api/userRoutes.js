f5351aec021e5a809b11f40f7525b86836257bc9
Green Camper <stefan.palacios@gmail.com>
2023-05-02 21:26:32 -0500

    Added login route and started logout route

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+//login route with email and password
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email }});
+    //if there is no email, throw error
+    if (!userData) {
+      return res.status(400).json({ message: 'Wrong email or password, try again.' });
+    }
+      const validPassword = await userData.checkPassword(req.body.password);
+    //if there is an invalid password, throw error
+    if (!validPassword) {
+      return res.status(400).json({ message: 'Wrong email or password, try again.' });
+    }
+    //function to save session data and produce success message
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'Logged in successfully!'});
+    })
+    
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+//logout route
+router.post('/logout')
+
