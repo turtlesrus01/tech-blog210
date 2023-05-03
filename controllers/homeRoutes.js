fc395f168dbee67d0c3cca4c1f6abebaba283129
Green Camper <stefan.palacios@gmail.com>
2023-05-02 21:02:16 -0500

    Added homeRoutes and api/index in controllers and main handlebars code

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+//Get route for retrieving user data
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']]
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+    res.render('homepage', {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//Get route for login
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
